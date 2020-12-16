0c3c3a65a33b328810e23d7cce205f6c6ed27d34
Elie NEHME <elie.nehme@soprasterianext.com>
2020-12-16 13:46:43 +0100

    add button

--- projects/first-lib/src/lib/feature-a/component-a/component-a.component.ts
+++ projects/first-lib/src/lib/feature-a/component-a/component-a.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'lib-component-a',
   templateUrl: './component-a.component.html',
-  styleUrls: ['./component-a.component.css']
+  styleUrls: ['./component-a.component.scss'],
 })
 export class ComponentAComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
